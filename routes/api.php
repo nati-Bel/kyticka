2c1385f4331962a300369dc696d0a5777cc973bd
natalia.belzarova <natalia.belzarova@gmail.com>
2023-09-13 11:35:04 +0200

    GalleryController, Model, Request, Ressource ok

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,12 @@ use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GalleryController;
 
 
 
-Route::middleware('example')->resource('/admin/photos', PhotoController::class);
+Route::resource('/admin/photos', PhotoController::class);
+Route::resource('/admin/galleries', GalleryController::class);
 
 Route::post('/create', [AuthController::class, 'createUser']);
 Route::post('/login', [AuthController::class, 'loginUser']);
